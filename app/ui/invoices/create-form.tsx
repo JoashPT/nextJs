abad3faa16ffe4495eab7ec3d85e01b383db8414
Joash Tantiangco <tan24001@byui.edu>
2025-07-23 19:12:58 +0800

    fixed some bugs in for types updateInvoice and createInvoice

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -13,6 +13,7 @@ import { useActionState } from 'react';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   const initialState: State = { message: null, errors: {} };
+  console.log(useActionState(createInvoice, initialState));
   const [state, formAction] = useActionState(createInvoice, initialState);
   
   return (
@@ -43,8 +44,8 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
           </div>
           <div id="customer-error" aria-live="polite" aria-atomic="true">
-            {state.errors?.customerId &&
-              state.errors.customerId.map((error: string) => (
+            {state.errors?.customersId &&
+              state.errors.customersId.map((error: string) => (
                 <p className="mt-2 text-sm text-red-500" key={error}>
                   {error}
                 </p>
